71cdbafc6470265433784aed20fab83963e4522e
NoleStites <nole.stites@gmail.com>
2025-06-08 19:41:57 -0500

    Supports different string amounts

--- scripts/pattern_preview.js
+++ scripts/pattern_preview.js
@@ -1,9 +1,10 @@
 function createPatternPreview(columns, rows, cell_overlap, row_overlap, strings) {
     let preview_section = document.getElementById("pattern_preview");
-    
+    strings /= 4;
+
     // Make a list of class numbers to be assigned to each cell in preview
     let color_class_values = [];
-    for (let i = 0; i < strings; i++) {
+    for (let i = 0; i < 4; i++) {
         let row = [];
         for (let j = 0; j < strings; j++) {
             row.push(i*strings + j);
@@ -14,26 +15,14 @@ function createPatternPreview(columns, rows, cell_overlap, row_overlap, strings)
     let curr_row_flipflop = 0;
     let start_color_offset = -1;
     for (let i = 0; i < rows; i++) {
-        let start_class_row = i % strings;
+        let start_class_row = i % 4;
 
         // Groups rows into pairs
         curr_row_flipflop = i % 2;
-        if (curr_row_flipflop == 0) {
-            start_color_offset += 1;
-            // if (start_color_offset == 4) {start_color_offset = 0;}
-            // if (start_color_offset == strings) {
-            //     // start_color_offset = 0;
-            //     start_class_row -= 1;
-            // }
-        }
-        // console.log("start_color_offset: ", start_color_offset);
+        if (curr_row_flipflop == 0) { start_color_offset += 1; }
         start_class_row -= Math.floor(start_color_offset/strings);
-        start_class_row %= strings;
-        // if (start_class_row < 0) { start_class_row = strings-1; }
-        // start_color_offset %= strings;
-
-        // let start_class_row = (i % strings) - 1;
-        console.log("start_class_row: ", start_class_row, "; start_color_offset: ", start_color_offset);
+        start_class_row %= 4;
+        if (start_class_row < 0) { start_class_row = 4+start_class_row; }
 
         // Create the row
         let new_row = document.createElement("div");
@@ -51,15 +40,10 @@ function createPatternPreview(columns, rows, cell_overlap, row_overlap, strings)
 
             let cell_to_access = ((j+start_color_offset) % strings);
             if ((cell_to_access == 0) && j != 0) {
-                // cell_to_access = 0;
                 curr_class_row -= 1;
             }
         
-            if (curr_class_row == -1) {curr_class_row = strings - 1;}
-            // console.log("curr_class_row: ", curr_class_row);
-            // if (j % strings == 0) {start_class_row += 1;}
-            // console.log(start_class_row % strings**2);
-            // new_cell.innerText = color_class_values[start_class_row % strings][cell_to_access];
+            if (curr_class_row == -1) {curr_class_row = 4 - 1;}
             let class_num = color_class_values[curr_class_row][cell_to_access];
             new_cell.innerText = class_num;
             new_cell.classList.add(`cell_${class_num}`);
@@ -76,7 +60,9 @@ let cell_overlap = Number(styles.getPropertyValue('--cell_overlap').slice(0,-2))
 let row_overlap = 15;
 let columns = 8;
 let rows = 15;
-// let columns = 13;
-// let rows = 23;
-let strings = 4;
+let strings = 16; // The number of pairs placed around the disc (16 is standard) (+/-4)
 createPatternPreview(columns, rows, cell_overlap, row_overlap, strings);
+
+// 16 => 4x4
+// 12 => 3x4
+// 8 => 2x4
