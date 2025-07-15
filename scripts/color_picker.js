37665cc1eb2ed5f12047783c48499f79653b69f3
NoleStites <nole.stites@gmail.com>
2025-07-14 19:50:03 -0500

    Adjusted body height

--- scripts/color_picker.js
+++ scripts/color_picker.js
@@ -1,6 +1,7 @@
 function setRealVh() {
   let vh = window.innerHeight * 0.01;
   document.documentElement.style.setProperty('--vh', `${vh}px`);
+  document.body.style.height = window.innerHeight + 'px';
 }
 
 window.addEventListener('resize', setRealVh);
